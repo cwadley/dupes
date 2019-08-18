2f31942a70d91d465ebc87191db6fbcd0e408444
Clinton Wadley <clinton-wadley@teletracking.com>
2019-08-17 21:37:08 -0400

    Renaming to dupes, switched to using xxhash and highwayhash in dual hashing mode. Refactoring

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-# dups
+# dupes
 A tool for finding duplicate files
 
 # How to build
 ```
 dep ensure
-go build dups.go
+go build dupes.go
 ```
 
 # How to run
-`./dups DIRECTORY`
+`./dupes DIRECTORY`
 
-The DIRECTORY argument should be a directory. dups will recursively walk all of the files in all subdirectories print out any duplicate files, based on their xxhash values.
+The DIRECTORY argument should be a directory. dupes will recursively walk all of the files in all subdirectories print out any duplicate files.
+
+dupes uses a dual hash to ensure collisions of a single hash do not result in false positive duplicates. Currently, xxhash is used as the primary hash, with highwayhash used as the secondary hash to verify duplicates.
---
+++ dupes.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/dghubble/trie"
+	"github.com/OneOfOne/xxhash"
+	"github.com/minio/highwayhash"
+	"gopkg.in/gookit/color.v1"
+)
+
+// Key used as seed for the HighwayHash algorithm.
+// This is hardcoded to ensure consistent hashes for files across runs.
+const HH_KEY = "E9ECA1531393D174DFEA70CC5BAA4FCE5FC599D08ECB36B9961489985A64D3AE"
+
+func PrintUsage() {
+	fmt.Println("Usage: dupes DIRECTORY")
+	fmt.Println("DIRECTORY is the directory that will be recursively searched for duplicate files")
+}
+
+func PrintDupes(t *trie.RuneTrie) {
+	t.Walk(
+		func(k string, d interface{}) error {
+			if d != nil {
+				dupes := d.([]string)
+				if len(dupes) > 1 {
+					color.Blue.Printf("Hash: %x\n", k)
+					for _, f := range dupes {
+						color.Yellow.Printf("\t%s\n", f)
+					}
+					fmt.Println()
+				}
+			}
+			return nil
+		})
+}
+
+func AddEntryToTrie(key string, path string, trie *trie.RuneTrie) {
+	dupes := make([]string, 1)
+	dupes[0] = path
+	trie.Put(key, dupes)
+}
+
+func GetSingleReader(path string) (io.Reader, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	r := bytes.NewReader(b)
+	return r, nil
+}
+
+func GetDualReaders(path string) (io.Reader, io.Reader, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, nil, err
+	}
+	r1 := bytes.NewReader(b)
+	r2 := bytes.NewReader(b)
+	return r1, r2, nil
+}
+
+func ComputeXXHash(r io.Reader) (string, error) {
+	h := xxhash.New64()
+	if _, err := io.Copy(h, r); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
+
+func ComputeHighwayHash(r io.Reader) (string, error) {
+	hhKey, err := hex.DecodeString(HH_KEY)
+	if err != nil {
+		return "", err
+	}
+
+	h, err := highwayhash.New(hhKey)
+	if err != nil {
+		return "", err
+	}
+
+	if _, err := io.Copy(h, r); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) != 1 {
+		PrintUsage()
+		os.Exit(1)
+	}
+
+	h1Trie := trie.NewRuneTrie()
+	h2Trie := trie.NewRuneTrie()
+	dupeCount := 0
+	err := filepath.Walk(args[0],
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if info.IsDir() {
+				return nil
+			}
+
+			r1, r2, err := GetDualReaders(path)
+			if err != nil {
+				fmt.Println("Error opening file %s", path)
+				return err
+			}
+
+			hash1String, err := ComputeXXHash(r1)
+			if err != nil {
+				fmt.Println("Error computing xxHash")
+				return err
+			}
+
+			if exists := h1Trie.Get(hash1String); exists != nil {
+				// Compute hash2 of previouly seen file and add it to the trie
+				r3, err := GetSingleReader(exists.(string))
+				if err != nil {
+					fmt.Println("Error opening file %s", path)
+				}
+
+				hash2StringPrevFile, err := ComputeHighwayHash(r3)
+				if err != nil {
+					fmt.Println("Error computing HighwayHash")
+					return err
+				}
+				AddEntryToTrie(hash1String + hash2StringPrevFile, exists.(string), h2Trie)
+
+				// Now compute hash2 of the current file
+				hash2String, err := ComputeHighwayHash(r2)
+				if err != nil {
+					fmt.Println("Error computing HighwayHash")
+					return err
+				}
+
+				if d := h2Trie.Get(hash1String + hash2String); d != nil {
+					dupeCount++
+					dupes := d.([]string)
+					dupes = append(dupes, path)
+					h2Trie.Put(hash1String + hash2String, dupes)
+				} else {
+					AddEntryToTrie(hash1String + hash2String, path, h2Trie)
+				}
+			} else {
+				h1Trie.Put(hash1String, path)
+			}
+			return nil
+		})
+
+	if err != nil {
+		fmt.Println("Error reading directory %s. Please ensure the directory exists.", args[0])
+		os.Exit(3)
+	}
+
+	if dupeCount > 0 {
+		color.Red.Printf("%d Files with duplicates found:\n", dupeCount)
+		PrintDupes(h2Trie)
+	} else {
+		color.Green.Println("No duplicate files exist in the specified directory.")
+	}
+
+}
